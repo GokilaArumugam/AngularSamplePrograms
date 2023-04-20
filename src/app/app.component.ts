af6d19dad40d2347b6c2c9227be3eceedcdba698
GokilaArumugam <gokigokila97513@gmail.com>
2023-04-20 16:11:23 +0530

    myFirstApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { style } from '@angular/animations';
+import {OnChanges,OnInit,DoCheck,OnDestroy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+  //template:`<div>{{title}}</div>`
 })
-export class AppComponent {
+export class AppComponent implements OnChanges,OnInit,DoCheck,OnDestroy {
   title = 'my-first-app';
+
+  headertitle="Welcome Girls";
+
+  ngOnChanges(){
+    console.log("on changes called");
+    
+  }
+  ngOnInit(){
+    console.log("on init called");
+  }
+  ngDoCheck(){
+    console.log("on docheck called");
+  }
+  ngOnDestroy(){
+    console.log("on destroy called");
+  }
 }
